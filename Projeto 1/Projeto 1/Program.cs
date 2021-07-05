ed98861f67f42924bca5ab4b5d14ca2fd44ff023
Miguelfc97 <86972177+Miguelfc97@users.noreply.github.com>
2021-07-05 15:57:05 +0100

    Add files via upload

---
+++ Projeto 1/Projeto 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Eu sou o " + args[0]);
+            Console.ReadKey();
+        }
+    }
+}
