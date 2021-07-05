a8701bc57549e60a4630a2175b3c68f6c1931892
Miguelfc97 <86972177+Miguelfc97@users.noreply.github.com>
2021-07-05 16:18:24 +0100

    Add files via upload

---
+++ Projeto 2/Projeto 2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Projeto_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(113, 12);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(124, 20);
+            this.txtnome.TabIndex = 0;
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(113, 51);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(124, 20);
+            this.txtresultado.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Resultado";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(138, 100);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(99, 34);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 276);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtnome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
