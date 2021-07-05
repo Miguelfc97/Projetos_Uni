83ea5aa86091d94077268ed533c6fc817efa0af5
Miguelfc97 <86972177+Miguelfc97@users.noreply.github.com>
2021-07-05 16:37:17 +0100

    Add files via upload

---
+++ Projeto 7/Projeto 7/fmrmuseu.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Projeto_7
+{
+    partial class fmrmuseu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtordem = new System.Windows.Forms.TextBox();
+            this.txtdescricao = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnfirst = new System.Windows.Forms.Button();
+            this.btnprev = new System.Windows.Forms.Button();
+            this.btnnext = new System.Windows.Forms.Button();
+            this.btnlast = new System.Windows.Forms.Button();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.btnupd = new System.Windows.Forms.Button();
+            this.btndel = new System.Windows.Forms.Button();
+            this.dtpdata = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // txtordem
+            // 
+            this.txtordem.Location = new System.Drawing.Point(145, 47);
+            this.txtordem.Name = "txtordem";
+            this.txtordem.Size = new System.Drawing.Size(131, 20);
+            this.txtordem.TabIndex = 0;
+            // 
+            // txtdescricao
+            // 
+            this.txtdescricao.Location = new System.Drawing.Point(145, 92);
+            this.txtdescricao.Name = "txtdescricao";
+            this.txtdescricao.Size = new System.Drawing.Size(131, 20);
+            this.txtdescricao.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ordem";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Descricao";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Data de aquisicao";
+            // 
+            // btnfirst
+            // 
+            this.btnfirst.Location = new System.Drawing.Point(64, 188);
+            this.btnfirst.Name = "btnfirst";
+            this.btnfirst.Size = new System.Drawing.Size(78, 30);
+            this.btnfirst.TabIndex = 6;
+            this.btnfirst.Text = "|<";
+            this.btnfirst.UseVisualStyleBackColor = true;
+            this.btnfirst.Click += new System.EventHandler(this.btnfirst_Click);
+            // 
+            // btnprev
+            // 
+            this.btnprev.Location = new System.Drawing.Point(145, 188);
+            this.btnprev.Name = "btnprev";
+            this.btnprev.Size = new System.Drawing.Size(78, 30);
+            this.btnprev.TabIndex = 7;
+            this.btnprev.Text = "<";
+            this.btnprev.UseVisualStyleBackColor = true;
+            this.btnprev.Click += new System.EventHandler(this.btnprev_Click);
+            // 
+            // btnnext
+            // 
+            this.btnnext.Location = new System.Drawing.Point(226, 188);
+            this.btnnext.Name = "btnnext";
+            this.btnnext.Size = new System.Drawing.Size(78, 30);
+            this.btnnext.TabIndex = 8;
+            this.btnnext.Text = ">";
+            this.btnnext.UseVisualStyleBackColor = true;
+            this.btnnext.Click += new System.EventHandler(this.btnnext_Click);
+            // 
+            // btnlast
+            // 
+            this.btnlast.Location = new System.Drawing.Point(307, 188);
+            this.btnlast.Name = "btnlast";
+            this.btnlast.Size = new System.Drawing.Size(78, 30);
+            this.btnlast.TabIndex = 9;
+            this.btnlast.Text = ">|";
+            this.btnlast.UseVisualStyleBackColor = true;
+            this.btnlast.Click += new System.EventHandler(this.btnlast_Click);
+            // 
+            // btnnew
+            // 
+            this.btnnew.Location = new System.Drawing.Point(96, 240);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(80, 39);
+            this.btnnew.TabIndex = 10;
+            this.btnnew.Text = "Novo";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // btnupd
+            // 
+            this.btnupd.Location = new System.Drawing.Point(182, 240);
+            this.btnupd.Name = "btnupd";
+            this.btnupd.Size = new System.Drawing.Size(80, 39);
+            this.btnupd.TabIndex = 11;
+            this.btnupd.Text = "Alterar";
+            this.btnupd.UseVisualStyleBackColor = true;
+            this.btnupd.Click += new System.EventHandler(this.btnupd_Click);
+            // 
+            // btndel
+            // 
+            this.btndel.Location = new System.Drawing.Point(268, 240);
+            this.btndel.Name = "btndel";
+            this.btndel.Size = new System.Drawing.Size(80, 39);
+            this.btndel.TabIndex = 12;
+            this.btndel.Text = "Apagar";
+            this.btndel.UseVisualStyleBackColor = true;
+            this.btndel.Click += new System.EventHandler(this.btndel_Click);
+            // 
+            // dtpdata
+            // 
+            this.dtpdata.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpdata.Location = new System.Drawing.Point(145, 129);
+            this.dtpdata.Name = "dtpdata";
+            this.dtpdata.Size = new System.Drawing.Size(93, 20);
+            this.dtpdata.TabIndex = 13;
+            // 
+            // fmrmuseu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 333);
+            this.Controls.Add(this.dtpdata);
+            this.Controls.Add(this.btndel);
+            this.Controls.Add(this.btnupd);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.btnlast);
+            this.Controls.Add(this.btnnext);
+            this.Controls.Add(this.btnprev);
+            this.Controls.Add(this.btnfirst);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtdescricao);
+            this.Controls.Add(this.txtordem);
+            this.Name = "fmrmuseu";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtordem;
+        private System.Windows.Forms.TextBox txtdescricao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnfirst;
+        private System.Windows.Forms.Button btnprev;
+        private System.Windows.Forms.Button btnnext;
+        private System.Windows.Forms.Button btnlast;
+        private System.Windows.Forms.Button btnnew;
+        private System.Windows.Forms.Button btnupd;
+        private System.Windows.Forms.Button btndel;
+        private System.Windows.Forms.DateTimePicker dtpdata;
+    }
+}
+
---
+++ Projeto 7/Projeto 7/fmrmuseu.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_7
+{
+    struct peca
+    {
+        public int nordem;
+        public string descricao;
+        public DateTime datacompra;
+    }
+
+    public partial class fmrmuseu : Form
+    {
+        private int i = 0;
+        private peca[] a = new peca[] { };
+
+        private void MostrarDados(peca p)
+        {
+            txtordem.Text = p.nordem.ToString();
+            txtdescricao.Text = p.descricao;
+            dtpdata.Value = p.datacompra;
+        }
+
+
+        public fmrmuseu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnfirst_Click(object sender, EventArgs e)
+        {
+            if (a.Length > 0)
+            {
+                i = 0;
+                MostrarDados(a[i]);
+            }
+            else
+                MessageBox.Show("Não há peças");
+        }
+
+        private void btnprev_Click(object sender, EventArgs e)
+        {
+            if (a.Length > 0)
+                if (i > 0)
+                {
+                    i--;
+                    MostrarDados(a[i]);
+                }
+                else
+                    MessageBox.Show("Não pode passar do primeiro");
+            else
+                MessageBox.Show("Não há peças");
+        }
+
+        private void btnnext_Click(object sender, EventArgs e)
+        {
+            if (a.Length > 0)
+                if (i < a.Length - 1)
+                {
+                    i++;
+                    MostrarDados(a[i]);
+                }
+                else
+                    MessageBox.Show("Não pode passar do ultimo");
+            else
+                MessageBox.Show("Não há peças");
+        }
+
+        private void btnlast_Click(object sender, EventArgs e)
+        {
+            if (a.Length > 0)
+            {
+                i = a.Length - 1;
+                txtordem.Text = a[i].nordem.ToString();
+                txtdescricao.Text = a[i].descricao;
+                dtpdata.Value = a[i].datacompra;
+            }
+            else
+                MessageBox.Show("Não há peças");
+        }
+
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            Array.Resize<peca>(ref a, a.Length + 1);
+            i = a.Length - 1;
+            a[i].nordem = int.Parse(txtordem.Text);
+            a[i].descricao = txtdescricao.Text;
+            a[i].datacompra = dtpdata.Value;
+        }
+
+        private void btnupd_Click(object sender, EventArgs e)
+        {
+            if (a.Length > 0)
+            {
+                a[i].nordem = int.Parse(txtordem.Text);
+                a[i].descricao = txtdescricao.Text;
+                a[i].datacompra = dtpdata.Value;
+            }
+        }
+
+        private void btndel_Click(object sender, EventArgs e)
+        {
+            if (a.Length > 0)
+            {
+                peca[] a2 = new peca[a.Length];
+                a.CopyTo(a2, 0);
+                a = new peca[a2.Length - 1];
+                for (int j = 0; j < i; j++)
+                    a[j] = a2[j];
+                for (int j = i + 1; j < a2.Length; j++)
+                    a[j - 1] = a2[j];
+                if (i > a.Length - 1) i = a.Length - 1;
+                if (a.Length > 0)
+                {
+                    txtordem.Text = a[i].nordem.ToString();
+                    txtdescricao.Text = a[i].descricao;
+                    dtpdata.Value = a[i].datacompra;
+                }
+                else
+                {
+                    txtordem.Text = "";
+                    txtdescricao.Text = "";
+                    dtpdata.Value = DateTime.Today;
+                }
+            }
+            else
+                MessageBox.Show("Não há peças");
+        }
+
+        private void ValidaNumero(object sender, CancelEventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            long i = 0;
+            if (!long.TryParse(txt.Text, out i))
+            {
+                MessageBox.Show("Valor invalido");
+                e.Cancel = true;
+            }
+        }
+    }
+}
