f0eb7eae024114f26b5396fc170e0800dee48fa4
Miguelfc97 <86972177+Miguelfc97@users.noreply.github.com>
2021-07-05 16:36:44 +0100

    Add files via upload

---
+++ Projeto 6/Projeto6/fmrAlunos.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Projeto6
+{
+    partial class fmrAlunos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnfirst = new System.Windows.Forms.Button();
+            this.btnprev = new System.Windows.Forms.Button();
+            this.btnnext = new System.Windows.Forms.Button();
+            this.btnlast = new System.Windows.Forms.Button();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.btnupd = new System.Windows.Forms.Button();
+            this.btndel = new System.Windows.Forms.Button();
+            this.txtnum = new System.Windows.Forms.TextBox();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtnota = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnfirst
+            // 
+            this.btnfirst.Location = new System.Drawing.Point(53, 158);
+            this.btnfirst.Name = "btnfirst";
+            this.btnfirst.Size = new System.Drawing.Size(41, 29);
+            this.btnfirst.TabIndex = 0;
+            this.btnfirst.Text = "|<";
+            this.btnfirst.UseVisualStyleBackColor = true;
+            this.btnfirst.Click += new System.EventHandler(this.btnfirst_Click);
+            // 
+            // btnprev
+            // 
+            this.btnprev.Location = new System.Drawing.Point(100, 158);
+            this.btnprev.Name = "btnprev";
+            this.btnprev.Size = new System.Drawing.Size(41, 29);
+            this.btnprev.TabIndex = 1;
+            this.btnprev.Text = "<";
+            this.btnprev.UseVisualStyleBackColor = true;
+            this.btnprev.Click += new System.EventHandler(this.btnprev_Click);
+            // 
+            // btnnext
+            // 
+            this.btnnext.Location = new System.Drawing.Point(147, 158);
+            this.btnnext.Name = "btnnext";
+            this.btnnext.Size = new System.Drawing.Size(41, 29);
+            this.btnnext.TabIndex = 2;
+            this.btnnext.Text = ">";
+            this.btnnext.UseVisualStyleBackColor = true;
+            this.btnnext.Click += new System.EventHandler(this.btnnext_Click);
+            // 
+            // btnlast
+            // 
+            this.btnlast.Location = new System.Drawing.Point(194, 158);
+            this.btnlast.Name = "btnlast";
+            this.btnlast.Size = new System.Drawing.Size(41, 29);
+            this.btnlast.TabIndex = 3;
+            this.btnlast.Text = ">|";
+            this.btnlast.UseVisualStyleBackColor = true;
+            this.btnlast.Click += new System.EventHandler(this.btnlast_Click);
+            // 
+            // btnnew
+            // 
+            this.btnnew.Location = new System.Drawing.Point(34, 210);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(69, 28);
+            this.btnnew.TabIndex = 4;
+            this.btnnew.Text = "Novo";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // btnupd
+            // 
+            this.btnupd.Location = new System.Drawing.Point(109, 210);
+            this.btnupd.Name = "btnupd";
+            this.btnupd.Size = new System.Drawing.Size(69, 28);
+            this.btnupd.TabIndex = 5;
+            this.btnupd.Text = "Alterar";
+            this.btnupd.UseVisualStyleBackColor = true;
+            this.btnupd.Click += new System.EventHandler(this.btnupd_Click);
+            // 
+            // btndel
+            // 
+            this.btndel.Location = new System.Drawing.Point(184, 210);
+            this.btndel.Name = "btndel";
+            this.btndel.Size = new System.Drawing.Size(69, 28);
+            this.btndel.TabIndex = 6;
+            this.btndel.Text = "Apagar";
+            this.btndel.UseVisualStyleBackColor = true;
+            this.btndel.Click += new System.EventHandler(this.btndel_Click);
+            // 
+            // txtnum
+            // 
+            this.txtnum.Location = new System.Drawing.Point(122, 23);
+            this.txtnum.Name = "txtnum";
+            this.txtnum.Size = new System.Drawing.Size(131, 20);
+            this.txtnum.TabIndex = 7;
+            this.txtnum.Validating += new System.ComponentModel.CancelEventHandler(this.ValidaNumero);
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(122, 64);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(131, 20);
+            this.txtnome.TabIndex = 8;
+            // 
+            // txtnota
+            // 
+            this.txtnota.Location = new System.Drawing.Point(122, 108);
+            this.txtnota.Name = "txtnota";
+            this.txtnota.Size = new System.Drawing.Size(131, 20);
+            this.txtnota.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Numero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Nota";
+            // 
+            // fmrAlunos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(308, 289);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtnota);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.txtnum);
+            this.Controls.Add(this.btndel);
+            this.Controls.Add(this.btnupd);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.btnlast);
+            this.Controls.Add(this.btnnext);
+            this.Controls.Add(this.btnprev);
+            this.Controls.Add(this.btnfirst);
+            this.Name = "fmrAlunos";
+            this.Text = "Alunos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnfirst;
+        private System.Windows.Forms.Button btnprev;
+        private System.Windows.Forms.Button btnnext;
+        private System.Windows.Forms.Button btnlast;
+        private System.Windows.Forms.Button btnnew;
+        private System.Windows.Forms.Button btnupd;
+        private System.Windows.Forms.Button btndel;
+        private System.Windows.Forms.TextBox txtnum;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtnota;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Projeto 6/Projeto6/fmrAlunos.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto6
+{
+    struct Aluno
+    {
+        public long numero;
+        public string nome;
+        public int nota;
+    }
+
+    public partial class fmrAlunos : Form
+    {
+        private int i = 0;
+        private Aluno[] a = new Aluno[] { };
+
+        private void MostrarDados(Aluno al)
+        {
+            txtnum.Text = al.numero.ToString();
+            txtnome.Text = al.nome;
+            txtnota.Text = al.nota.ToString();
+        }
+
+
+        public fmrAlunos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnfirst_Click(object sender, EventArgs e)
+        {
+            if (a.Length > 0)
+            {
+                i = 0;
+                //txtnum.Text = a[i].numero.ToString();
+                //txtnome.Text = a[i].nome;
+                //txtnota.Text = a[i].nota.ToString();
+                MostrarDados(a[i]);
+            }
+            else
+                MessageBox.Show("Não há alunos");
+        }
+
+        private void btnprev_Click(object sender, EventArgs e)
+        {
+            if (a.Length > 0)
+                if (i>0)
+                {
+                    i--;
+                    //txtnum.Text = a[i].numero.ToString();
+                    //txtnome.Text = a[i].nome;
+                    //txtnota.Text = a[i].nota.ToString();
+                    MostrarDados(a[i]);
+                 }
+                    else
+                        MessageBox.Show("Não pode passar do primeiro");
+            else
+                MessageBox.Show("Não há alunos");
+        }
+
+        private void btnnext_Click(object sender, EventArgs e)
+        {
+            if (a.Length > 0)
+                if (i < a.Length - 1)
+                {
+                    i++;
+                    //txtnum.Text = a[i].numero.ToString();
+                    //txtnome.Text = a[i].nome;
+                    //txtnota.Text = a[i].nota.ToString();
+                    MostrarDados(a[i]);
+                }
+                else
+                    MessageBox.Show("Não pode passar do ultimo");
+            else
+                MessageBox.Show("Não há alunos");
+        }
+
+        private void btnlast_Click(object sender, EventArgs e)
+        {
+            if (a.Length > 0)
+            {
+                i = a.Length-1;
+                txtnum.Text = a[i].numero.ToString();
+                txtnome.Text = a[i].nome;
+                txtnota.Text = a[i].nota.ToString();
+            }
+            else
+                MessageBox.Show("Não há alunos");
+        }
+
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            Array.Resize<Aluno>(ref a, a.Length + 1);
+            i = a.Length - 1;
+            a[i].numero = long.Parse(txtnum.Text);
+            a[i].nome = txtnome.Text;
+            a[i].nota = int.Parse(txtnota.Text);
+            
+        }
+
+        private void btnupd_Click(object sender, EventArgs e)
+        {
+            if(a.Length>0)
+            {
+                a[i].numero = long.Parse(txtnum.Text);
+                a[i].nome = txtnome.Text;
+                a[i].nota = int.Parse(txtnota.Text);
+            }
+        }
+
+        private void btndel_Click(object sender, EventArgs e)
+        {
+            if(a.Length>0)
+            {
+                Aluno[] a2 = new Aluno[a.Length];
+                a.CopyTo(a2, 0);
+                a = new Aluno[a2.Length-1];
+                for (int j = 0; j < i; j++)
+                    a[j] = a2[j];
+                for (int j = i + 1; j < a2.Length; j++)
+                    a[j - 1] = a2[j];
+                if (i > a.Length - 1) i = a.Length - 1;
+                if (a.Length > 0)
+                {
+                    txtnum.Text = a[i].numero.ToString();
+                    txtnome.Text = a[i].nome;
+                    txtnota.Text = a[i].nota.ToString();
+                }
+                else
+                {
+                    txtnum.Text = "";
+                    txtnome.Text = "";
+                    txtnota.Text = "";
+                }
+            }
+            else
+                MessageBox.Show("Não há alunos");
+        }
+
+        private void ValidaNumero(object sender, CancelEventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            long i = 0;
+            if (!long.TryParse(txt.Text, out i))
+            {
+                MessageBox.Show("Valor invalido");
+                e.Cancel = true;
+            }
+        }
+    }
+}
