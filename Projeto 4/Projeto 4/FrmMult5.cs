11e59ca8e23c19e90ca0b311531d1be6d8595fda
Miguelfc97 <86972177+Miguelfc97@users.noreply.github.com>
2021-07-05 16:27:02 +0100

    Add files via upload

---
+++ Projeto 4/Projeto 4/FrmMult5.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_4
+{
+    public partial class FrmMult5 : Form
+    {
+        public FrmMult5()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            int n1, n2, n3, mult;
+            mult = int.Parse(txtmult.Text);
+            n1 = int.Parse(txtliminf.Text);
+            n2 = int.Parse(txtlimsup.Text);
+            n3 = (n2 - n1) / mult;
+            if ((n3 % mult) == 0)
+                n3--;
+            if ((n1 % mult) == 0)
+                n3++;
+            if ((n2 % mult) == 0)
+                n3++;
+            //n3=0;
+            //for (int i = n1; i <= n2; i++)
+            //    if ((i % mult) == 0)
+            //        n3++; 
+
+            lblresultado.Text = "O numero de multiplos de " + mult.ToString() + " entre " + n1.ToString() + " e " + n2.ToString() + " é: " + n3.ToString();
+        }
+    }
+}
---
+++ Projeto 4/Projeto 4/frmmult5.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Projeto_4
+{
+    partial class FrmMult5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.txtlimsup = new System.Windows.Forms.TextBox();
+            this.txtliminf = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.txtmult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Limite Superior";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Limite Inferior";
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(60, 173);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(55, 13);
+            this.lblresultado.TabIndex = 2;
+            this.lblresultado.Text = "Resultado";
+            // 
+            // txtlimsup
+            // 
+            this.txtlimsup.Location = new System.Drawing.Point(140, 90);
+            this.txtlimsup.Name = "txtlimsup";
+            this.txtlimsup.Size = new System.Drawing.Size(100, 20);
+            this.txtlimsup.TabIndex = 3;
+            // 
+            // txtliminf
+            // 
+            this.txtliminf.Location = new System.Drawing.Point(140, 52);
+            this.txtliminf.Name = "txtliminf";
+            this.txtliminf.Size = new System.Drawing.Size(100, 20);
+            this.txtliminf.TabIndex = 4;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(165, 190);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 5;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // txtmult
+            // 
+            this.txtmult.Location = new System.Drawing.Point(140, 129);
+            this.txtmult.Name = "txtmult";
+            this.txtmult.Size = new System.Drawing.Size(100, 20);
+            this.txtmult.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Multiplo a calcular";
+            // 
+            // FrmMult5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 258);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtmult);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtliminf);
+            this.Controls.Add(this.txtlimsup);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMult5";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.TextBox txtlimsup;
+        private System.Windows.Forms.TextBox txtliminf;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.TextBox txtmult;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
